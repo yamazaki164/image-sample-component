4f967aaccff07236ba75453904ab3b71feb208ff
yamazaki164 <yamazaki164@gmail.com>
2019-08-21 00:12:37 +0900

    add rotate event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,14 @@ export class AppComponent {
   title = 'img';
 
   image: HTMLImageElement;
-  canvas: HTMLCanvasElement;
+  image2: HTMLImageElement;
   isLoad: boolean = false;
   isError: boolean = false;
+  isLoad2: boolean = false;
 
   constructor() {
     this.image = new Image();
-    this.canvas = document.createElement("canvas");
+    this.image2 = new Image();
   }
 
   canShowImage(): boolean {
@@ -39,12 +40,37 @@ export class AppComponent {
     });
   }
 
+  rotate(angle: number) {
+    const image = this.image2;
+    this.image2.src = this.rotateImage(angle, image);
+  }
+
+  private rotateImage(angle: number, image: HTMLImageElement): string {
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext("2d");
+
+    canvas.width = image.height;
+    canvas.height = image.width;
+
+    context.translate(image.height/2, image.width/2);
+    context.rotate(angle * Math.PI / 180);
+
+    context.drawImage(image, -image.width/2, -image.height/2);
+
+    return canvas.toDataURL();
+  }
+
   private readImage(buffer: string | ArrayBuffer) {
     this.image.src = buffer.toString();
 
     this.image.onload = () => {
       this.isLoad = true;
       this.isError = false;
+
+      this.image2.src = buffer.toString();
+      this.image2.onload = () => {
+        this.isLoad2 = true;
+      }
     }
 
     this.image.onerror = () => {
